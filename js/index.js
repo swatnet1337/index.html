a1259ec440d44d4e59067e10be0a6a0c3d87821e
swatnet1337 <118098220+swatnet1337@users.noreply.github.com>
2022-11-13 12:59:09 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -104,7 +104,7 @@ function ibu_enter( )
         "particles": {
             "number": { "value": 20, "density": { "enable": true, "value_area": 450 } },
             "color": { "value": "#ffffff" },
-            "shape": { "type": "image", "image": { "src": "dep/logo.png", "width": 100, "height": 100} },
+            "shape": { "type": "image", "image": { "src": "dep/logo.svg", "width": 100, "height": 100} },
             "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } },
             "size": { "value": 30, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } },
             "move": { "enable": true, "speed": 2, "direction": "bottom", "random": false, "straight": false, "out_mode": "out", "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } },
